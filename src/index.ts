cba2d08e3163bfd0148b93f1b636533480c80510
radumir0n <radu.miron@equilobe.com>
2023-02-11 14:47:07 +0200

    chore: initial commit, project setup

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import * as dotenv from 'dotenv';
+
+const app = express();
+const { PORT } = process.env;
+
+dotenv.config();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'Hello Todues!' });
+});
+
+app.listen(PORT || 8080, async () => {
+    console.log(`Server is running on port ${PORT || 8080}`);
+});
